e6353701fc6ec912062f306be733da190e41d8f8
Gabrielli Tainá <141343996+12Gabrielli@users.noreply.github.com>
2025-07-07 11:14:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,99 +1,113 @@
-//criação das constantes para manipular os elementos HTML
-
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-//lista de perguntas com os objetos(itens) e seus atributos:enunciado, e lista de alternativas
-//com os atributos texto e afirmação
+
 const perguntas = [
     {
-        enunciado: "Quais práticas tradicionais africanas utilizam ervas medicinais e também envolvem o uso de substâncias psicoativas na cultura local?
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
             {
-            texto: "Rituais de iniciação que usam ervas medicinais e substâncias naturais para induzir estados alterados de consciência.
-            afirmacao: "verdadeira" 
+                texto: "Isso é assustador!",
+                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
             },
             {
-            texto: "Uso de chás de ervas para tratamentos de saúde, sem qualquer relação com práticas espirituais ou uso de drogas.
-            afirmacao: "verdadeira" 
-            },
+                texto: "Isso é maravilhoso!",
+                afirmacao: "Quis saber como usar IA no seu dia a dia."
+            }
         ]
     },
     {
-        enunciado: "Quais elementos são comuns na cultura africana ao integrar o uso de ervas medicinais e o consumo de drogas tradicionais? 
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
         alternativas: [
             {
-            texto: "Rituais de cura que utilizam ervas medicinais e substâncias tradicionais para equilibrar o corpo e a mente. 
-            afirmacao: "verdadeira"
+                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
             },
             {
-            texto: "Práticas de consumo de drogas ilícitas sem relação com a medicina tradicional ou rituais espirituais. 
-            afirmacao: "verdadeira" 
-            },
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+            }
         ]
     },
     {
-        enunciado: "Quais aspectos da cultura africana envolvem o uso de ervas medicinais e também o consumo de substâncias que podem ser consideradas drogas? 
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
         alternativas: [
             {
-            texto: " Festivais e rituais que utilizam ervas medicinais e substâncias psicoativas para promover a conexão espiritual 
-            afirmacao: "verdadeira" 
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
             },
             {
-            texto: "Uso de remédios naturais para tratamentos de saúde, sem qualquer uso de drogas ou substâncias psicoativas. 
-            afirmacao: "verdadeira"
-            },
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+            }
         ]
     },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+            }
+        ]
+    },
+    {
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        alternativas: [
+            {
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+            },
+            {
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+            }
+        ]
+    },
+];
 
-];//criação das variáveis atual(que percorrerá os itens da lista de perguntas)
-// perguntaAtual(que guardará a pergunta atual que será interagida)
-//historiaFinal(que inicia vazia e depois guardará os textos da resposta selecionada)
 
 let atual = 0;
 let perguntaAtual;
-let historiaFinal = "";//funcao que mostrará cada pergunta até que apareça encerre a lista e mostrará o resumo da I.A.
+let historiaFinal = "";
 
-function mostraPergunta(){
-    if (atual >= perguntas.length){
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
         mostraResultado();
         return;
     }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent = "";
-    textoResultado.textContent = "";
-//execução da função que mostrará as alternativas
-    mostraAlternativa();
-}//funcao que mostrará as alternativas do objeto selecionado
-
-function mostraAlternativa(){
-    for(const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativa = document.createElement("button");
-        botaoAlternativa.textContent = alternativa.texto;
-//execução da função SETA => que após o evento de clique selecionada a resposta da alternativa
-        botaoAlternativa.addEventListener("click",()=>respostaSelecionada(alternativa))
-        caixaAlternativas.appendChild(botaoAlternativa);
-   
+    mostraAlternativas();
 }
-}//função que junta todas as afirmações das alternativas clicadas.
 
-function respostaSelecionada(opcaoSelecionada){
-    const afirmacao = opcaoSelecionada.afirmacao;
-    historiaFinal += afirmacao + " ";
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
     atual++;
-//execução da função que mostra a pergunta
     mostraPergunta();
+}
 
-}//função que mostrará o resultado final 
-
-function mostraResultado(){
-    caixaPerguntas.textContent = "Conclusão...";
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
-
-}//execução da função que mostrará a pergunta e suas alternativas
+}
 
 mostraPergunta();
